94dbf3c95a525623ab7ef392a67872cfd1c90f66
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-08 13:30:03 +0530

    finish setup

--- alerts/views.py
+++ alerts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'home.html', {})
+    return render(request,'alerts/home.html', {})
