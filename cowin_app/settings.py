c310a4118fde01d713958b939e3e57f4e9829943
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-08 12:54:07 +0530

    setup apps

--- cowin_app/settings.py
+++ cowin_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hsnz!mc!y46_m&+b%6jmf6tyb#8x19#81y*+!@i%bwq#@dh(wp'
+SECRET_KEY = 'django-insecure-(wb1z4_s^(8m=+bk2!y4$5h@v@s$4j23b4!6lo&h#(hfi8#o1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
