a985ebedad508550d08d3904e1d1b46b114451e9
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-08 13:29:33 +0530

    finish setup

--- cowin/asgi.py
+++ cowin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cowin_app project.
+ASGI config for cowin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cowin_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cowin.settings')
 
 application = get_asgi_application()
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+class User(AbstractUser):
+    EMAIL_VERIFIED = 0
+    OTP_VERIFIED = 1
+    NOT_VERIFIED = 2
 
+
+    AUTH_VERIFICATION_STATUS = (
+        (EMAIL_VERIFIED, 'Email is verified'),
+        (OTP_VERIFIED, 'Phone number is verified'),
+        (NOT_VERIFIED, 'Not verified'),
+
+    )
+
+    username = models.CharField(max_length=100, unique=True,  blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    avatar = models.ImageField(
+        upload_to='',  max_length=1000, blank=True, null=True)
+    verification_status = models.IntegerField(
+        choices=AUTH_VERIFICATION_STATUS, default=3, blank=True, null=True)
 # Create your models here.
