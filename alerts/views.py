f39bf49822ce02f1fbaaf5cd7978ea82de3d254f
Shreyansh Singh <shreyansh071299@gmail.com>
2021-05-09 11:34:21 +0530

    failed post req

--- alerts/views.py
+++ alerts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
     return render(request,'alerts/home.html', {})
+
+def register_visitors(request):
+    if request.method == 'POST':
+        data = request.POST.dict()
+        try:
+            Visitors.objects.create(**data)
+            return HttpResponse(200)
+        except:
+            return HttpResponse(400)
+    return HttpResponse(400)
