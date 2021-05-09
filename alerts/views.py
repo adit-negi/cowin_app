cff4e51f931bd7ac43e02b0cd918a191c65f45b1
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-09 17:16:52 +0530

    add form data to post

--- alerts/views.py
+++ alerts/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+import json
 # Create your views here.
 def home(request):
     return render(request,'alerts/home.html', {})
 
 def register_visitors(request):
     if request.method == 'POST':
-        print(request.POST)
         data = request.POST.dict()
         print(data)
-        try:
-            Visitors.objects.get_or_create(**data)
-            return HttpResponse(200)
-        except:
-            return HttpResponse(400)
+  
+        obj, created = Visitor.objects.get_or_create(**data)
+        if not created:
+            return HttpResponse(400, "Already in database")
+        return HttpResponse(200)
+       
     return HttpResponse(400)
