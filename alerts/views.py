fbf45c8e61117f470fcc176da944c5e9279a700c
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-10 11:54:29 +0530

    add welcome mail

--- alerts/views.py
+++ alerts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 import json
+from django.core.mail import send_mail
 # Create your views here.
 def home(request):
     return render(request,'alerts/home_page.html', {})
@@ -14,6 +15,12 @@ def register_visitors(request):
         obj, created = Visitor.objects.get_or_create(**data)
         if not created:
             return HttpResponse(403, "Already in database")
+        if created:
+            qs = Visitor.objects.filter(email= obj.email)
+            if qs.count()==1 and qs[0].registration_mail_sent==False:
+                obj.registration_mail_sent=True
+                to = [obj.email, 'aditnegi1@gmail.com']
+                send_mail('No Reply', 'Your registration for cowin bot was confirmed, stay safe.','aditnegi8899@gmail.com', to)
         return HttpResponse(200)
        
     return HttpResponse(400)
